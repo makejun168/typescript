ffe881087461f55329be65398c5ec6a797ba47d1
makejun168 <873800030@qq.com>
2018-07-05 20:58:48 +0800

    'base'

--- base/helloworld.js
+++ base/helloworld.js
@@ -1,7 +1,12 @@
-// TypeScript 编译的时候即使报错了，还是会生成编译结果，我们仍然可以使用这个编译之后的文件。
 function sayHello(person) {
     return 'Hello, ' + person;
 }
-// let user = 'Tom';
-var user = [0, 1, 2];
+// let user = [0, 1, 2];
+var user = 'me';
 console.log(sayHello(user));
+var data = true;
+var myString = '123';
+var mytest = 666;
+function voidFun(data) {
+    alert('hello world');
+}
--- base/helloworld.ts
+++ base/helloworld.ts
@@ -2,6 +2,14 @@ function sayHello(person: string) {
     return 'Hello, ' + person;
 }
 
-let user = 'Tom';
 // let user = [0, 1, 2];
+let user: string = 'me';
 console.log(sayHello(user));
+
+let data: boolean = true;
+let myString: string = '123';
+let mytest: number = 666;
+
+function voidFun(data: number): void {
+    alert('hello world');
+}
